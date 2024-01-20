855ba026fd76ab8a7b101f79d8f83d602f97bdf6
Fabian Andrade <lufab96@gmail.com>
2024-01-20 00:15:40 -0500

    uso de svg

--- resources/index.js
+++ resources/index.js
@@ -10,7 +10,7 @@ const MULTIMEDIA = {
             plataformasMusica: [
                 { nombre: "Spotify", url: "https://open.spotify.com/track/3Q6G1iAlw1Cws3RDE96cDL" },
                 { nombre: "YouTube", url: "https://music.youtube.com/watch?v=nqlkZ2DTB_A&si=eD9rs4j5foER_FW_" },
-                { nombre: "Apple Music", url: "https://music.apple.com/us/album/sobrenatural-feat-addisong/1717492942?i=1717492943" }
+                { nombre: "Apple", url: "https://music.apple.com/us/album/sobrenatural-feat-addisong/1717492942?i=1717492943" }
             ],
             plataformasVideo: [],
         },
@@ -23,7 +23,7 @@ const MULTIMEDIA = {
             plataformasMusica: [
                 { nombre: "Spotify", url: "https://open.spotify.com/track/62JHWhcS2ABNSsve3K7jWg" },
                 { nombre: "YouTube", url: "https://music.youtube.com/watch?v=BdHwIT-m1UY&si=QHTelwTnLW9c0MJd" },
-                { nombre: "Apple Music", url: "https://music.apple.com/us/album/primera-vez-feat-fernando-ville/1715099089?i=1715099090" }
+                { nombre: "Apple", url: "https://music.apple.com/us/album/primera-vez-feat-fernando-ville/1715099089?i=1715099090" }
             ],
             plataformasVideo: [],
         },
@@ -36,11 +36,11 @@ const MULTIMEDIA = {
             plataformasMusica: [
                 { nombre: "Spotify", url: "https://open.spotify.com/track/5ShabTACl2O5tXVFrClNGE" },
                 { nombre: "YouTube", url: "https://music.youtube.com/watch?v=83hsamJu_Qk&si=2S1FHXtLfXWB_ROY" },
-                { nombre: "Apple Music", url: "https://music.apple.com/us/album/ser-un-ni%C3%B1o-otra-vez-feat-addisong/1708634683?i=1708634685" }
+                { nombre: "Apple", url: "https://music.apple.com/us/album/ser-un-ni%C3%B1o-otra-vez-feat-addisong/1708634683?i=1708634685" }
             ],
             plataformasVideo: [
                 { nombre: "YouTube", url: "https://youtu.be/-xOs7F91hjQ?si=Ra1gkcD5roDwAR9L" },
-                { nombre: "Apple Music", url: "https://music.apple.com/us/music-video/ser-un-ni%C3%B1o-otra-vez-feat-addisong/1722866849" }
+                { nombre: "Apple", url: "https://music.apple.com/us/music-video/ser-un-ni%C3%B1o-otra-vez-feat-addisong/1722866849" }
             ]
         }
     ],
@@ -68,6 +68,24 @@ const MERCANCIAS = {
             enlaceComprar: 'https://www.etsy.com/listing/1584866763/official-averos-project-cap',
         }
     ]
+}
+
+const ICONOS = {
+    spotify: {
+        xmlns: "http://www.w3.org/2000/svg",
+        viewBox: "0 0 496 512",
+        d: "M248 8C111.1 8 0 119.1 0 256s111.1 248 248 248 248-111.1 248-248S384.9 8 248 8zm100.7 364.9c-4.2 0-6.8-1.3-10.7-3.6-62.4-37.6-135-39.2-206.7-24.5-3.9 1-9 2.6-11.9 2.6-9.7 0-15.8-7.7-15.8-15.8 0-10.3 6.1-15.2 13.6-16.8 81.9-18.1 165.6-16.5 237 26.2 6.1 3.9 9.7 7.4 9.7 16.5s-7.1 15.4-15.2 15.4zm26.9-65.6c-5.2 0-8.7-2.3-12.3-4.2-62.5-37-155.7-51.9-238.6-29.4-4.8 1.3-7.4 2.6-11.9 2.6-10.7 0-19.4-8.7-19.4-19.4s5.2-17.8 15.5-20.7c27.8-7.8 56.2-13.6 97.8-13.6 64.9 0 127.6 16.1 177 45.5 8.1 4.8 11.3 11 11.3 19.7-.1 10.8-8.5 19.5-19.4 19.5zm31-76.2c-5.2 0-8.4-1.3-12.9-3.9-71.2-42.5-198.5-52.7-280.9-29.7-3.6 1-8.1 2.6-12.9 2.6-13.2 0-23.3-10.3-23.3-23.6 0-13.6 8.4-21.3 17.4-23.9 35.2-10.3 74.6-15.2 117.5-15.2 73 0 149.5 15.2 205.4 47.8 7.8 4.5 12.9 10.7 12.9 22.6 0 13.6-11 23.3-23.2 23.3z",
+    },
+    youtube: {
+        xmlns: "http://www.w3.org/2000/svg",
+        viewBox: "0 0 576 512",
+        d: "M549.7 124.1c-6.3-23.7-24.8-42.3-48.3-48.6C458.8 64 288 64 288 64S117.2 64 74.6 75.5c-23.5 6.3-42 24.9-48.3 48.6-11.4 42.9-11.4 132.3-11.4 132.3s0 89.4 11.4 132.3c6.3 23.7 24.8 41.5 48.3 47.8C117.2 448 288 448 288 448s170.8 0 213.4-11.5c23.5-6.3 42-24.2 48.3-47.8 11.4-42.9 11.4-132.3 11.4-132.3s0-89.4-11.4-132.3zm-317.5 213.5V175.2l142.7 81.2-142.7 81.2z",
+    },
+    apple: {
+        xmlns: "http://www.w3.org/2000/svg",
+        viewBox: "0 0 384 512",
+        d: "M318.7 268.7c-.2-36.7 16.4-64.4 50-84.8-18.8-26.9-47.2-41.7-84.7-44.6-35.5-2.8-74.3 20.7-88.5 20.7-15 0-49.4-19.7-76.4-19.7C63.3 141.2 4 184.8 4 273.5q0 39.3 14.4 81.2c12.8 36.7 59 126.7 107.2 125.2 25.2-.6 43-17.9 75.8-17.9 31.8 0 48.3 17.9 76.4 17.9 48.6-.7 90.4-82.5 102.6-119.3-65.2-30.7-61.7-90-61.7-91.9zm-56.6-164.2c27.3-32.4 24.8-61.9 24-72.5-24.1 1.4-52 16.4-67.9 34.9-17.5 19.8-27.8 44.3-25.6 71.9 26.1 2 49.9-11.4 69.5-34.3z"
+    }
 }
 
 const botonDisponibles = document.querySelector(".botonDisponibles")
@@ -152,15 +170,24 @@ function generarListado() {
                 const contenido = `
                         <img src="${media.coverURL}">
                         <div>
-                        <h3>${media.artistas}</h3>
-                        <p>${media.nombreCancion}</p>
+                            <h3>${media.artistas}</h3>
+                            <p>${media.nombreCancion}</p>
                         </div>
                         <div class="plataformas">
-                        ${media.plataformasMusica.map(plataforma => `
+                        ${media.plataformasMusica.map(plataforma => {
+                    const iconoPlataforma = ICONOS[plataforma.nombre.toLowerCase()];
+                    if (iconoPlataforma) {
+                        return `
                         <a href="${plataforma.url}" target="_blank" rel="noopener noreferrer">
-                        <i class="fab fa-${plataforma.nombre.toLowerCase()}"></i>
+                            <svg xmlns="${iconoPlataforma.xmlns}" viewBox="${iconoPlataforma.viewBox}">
+                                <path d="${iconoPlataforma.d}"/></path>
+                            </svg>
                         </a>
-                        `).join('')}
+                        `;
+                    } else {
+                        console.error(`La plataforma ${plataforma.nombre.toLowerCase()} no está definida en ICONOS.`);
+                        return ''; // Puedes ajustar esto según tus necesidades
+                    }}).join('')}
                         </div>
                         `
                 single.innerHTML = contenido
@@ -190,11 +217,20 @@ function generarListado() {
                         <p>${media.nombreCancion}</p>
                         </div>
                         <div class="plataformas">
-                        ${media.plataformasVideo.map(plataforma => `
+                        ${media.plataformasVideo.map(plataforma => {
+                    const iconoPlataforma = ICONOS[plataforma.nombre.toLowerCase()];
+                    if (iconoPlataforma) {
+                        return `
                         <a href="${plataforma.url}" target="_blank" rel="noopener noreferrer">
-                        <i class="fab fa-${plataforma.nombre.toLowerCase()}"></i>
+                            <svg xmlns="${iconoPlataforma.xmlns}" viewBox="${iconoPlataforma.viewBox}">
+                                <path d="${iconoPlataforma.d}"/></path>
+                            </svg>
                         </a>
-                        `).join('')}
+                        `;
+                    } else {
+                        console.error(`La plataforma ${plataforma.nombre.toLowerCase()} no está definida en ICONOS.`);
+                        return ''; // Puedes ajustar esto según tus necesidades
+                    }}).join('')}
                         </div>
                         `
                 singleVid.innerHTML = contenidoVid
@@ -223,17 +259,13 @@ generarListado();
 
 // funcion del boton drop y creacion de carroules 
 
-const botonDropUpTab = document.querySelector('.botonMostrarOcultar')
-const infoTab = document.querySelector('.infoTab')
+const botonDropUpTab = document.querySelector('.botonMostrarOcultar');
+const infoTab = document.querySelector('.infoTab');
 
-botonDropUpTab.addEventListener('click', mostrarOcultarInformacion)
+botonDropUpTab.addEventListener('click', mostrarOcultarInformacion);
 
 function mostrarOcultarInformacion() {
-    if (infoTab.style.display === 'none') {
-        botonDropUpTab.classList.replace('fa-chevron-circle-down', 'fa-chevron-circle-up')
-        infoTab.style.display = 'grid'
-    } else {
-        botonDropUpTab.classList.replace('fa-chevron-circle-up', 'fa-chevron-circle-down')
-        infoTab.style.display = 'none'
-    }
+    botonDropUpTab.classList.toggle('fa-chevron-circle-down');
+    botonDropUpTab.classList.toggle('fa-chevron-circle-up');
+    infoTab.style.display = infoTab.style.display === 'none' ? 'grid' : 'none';
 }
