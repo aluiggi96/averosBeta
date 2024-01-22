7b1fb736162d0af7a12ebb33418dc34a552cf3bd
Fabian Andrade <lufab96@gmail.com>
2024-01-22 15:32:55 -0500

    mejoras en la pagina

--- resources/index.js
+++ resources/index.js
@@ -1,3 +1,5 @@
+import { ICONOS } from "./logos/logosIconos.js";
+
 // Datos multimedia (musicas y videos) en forma de objetos
 const MULTIMEDIA = {
     DISPONIBLES: [
@@ -95,33 +97,6 @@ const MERCANCIAS = [
     },
 ];
 
-
-
-const ICONOS = {
-    xmlns: "http://www.w3.org/2000/svg",
-    spotify: {
-        viewBox: "0 0 496 512",
-        d: 'M248 8C111.1 8 0 119.1 0 256s111.1 248 248 248 248-111.1 248-248S384.9 8 248 8zm100.7 364.9c-4.2 0-6.8-1.3-10.7-3.6-62.4-37.6-135-39.2-206.7-24.5-3.9 1-9 2.6-11.9 2.6-9.7 0-15.8-7.7-15.8-15.8 0-10.3 6.1-15.2 13.6-16.8 81.9-18.1 165.6-16.5 237 26.2 6.1 3.9 9.7 7.4 9.7 16.5s-7.1 15.4-15.2 15.4zm26.9-65.6c-5.2 0-8.7-2.3-12.3-4.2-62.5-37-155.7-51.9-238.6-29.4-4.8 1.3-7.4 2.6-11.9 2.6-10.7 0-19.4-8.7-19.4-19.4s5.2-17.8 15.5-20.7c27.8-7.8 56.2-13.6 97.8-13.6 64.9 0 127.6 16.1 177 45.5 8.1 4.8 11.3 11 11.3 19.7-.1 10.8-8.5 19.5-19.4 19.5zm31-76.2c-5.2 0-8.4-1.3-12.9-3.9-71.2-42.5-198.5-52.7-280.9-29.7-3.6 1-8.1 2.6-12.9 2.6-13.2 0-23.3-10.3-23.3-23.6 0-13.6 8.4-21.3 17.4-23.9 35.2-10.3 74.6-15.2 117.5-15.2 73 0 149.5 15.2 205.4 47.8 7.8 4.5 12.9 10.7 12.9 22.6 0 13.6-11 23.3-23.2 23.3z',
-    },
-    ytmusic: {
-        viewBox: "0 0 100 100",
-        d: `M100 50C100 77.6142 77.6142 100 50 100C22.3858 100 0 77.6142 0 50C0 22.3858 22.3858 0 50 0C77.6142 0 100 22.3858 100 50ZM23.75 50C23.75 64.4975 35.5025 76.25 50 76.25C64.4975 76.25 76.25 64.4975 76.25 50C76.25 35.5025 64.4975 23.75 50 23.75C35.5025 23.75 23.75 35.5025 23.75 50Z"/>
-                                        <path fill-rule="evenodd" clip-rule="evenodd" d="M50 74C63.2548 74 74 63.2548 74 50C74 36.7452 63.2548 26 50 26C36.7452 26 26 36.7452 26 50C26 63.2548 36.7452 74 50 74ZM63 49.5L41 37V63L63 49.5Z" />`
-    },
-    youtube: {
-        viewBox: "0 0 576 512",
-        d: "M549.7 124.1c-6.3-23.7-24.8-42.3-48.3-48.6C458.8 64 288 64 288 64S117.2 64 74.6 75.5c-23.5 6.3-42 24.9-48.3 48.6-11.4 42.9-11.4 132.3-11.4 132.3s0 89.4 11.4 132.3c6.3 23.7 24.8 41.5 48.3 47.8C117.2 448 288 448 288 448s170.8 0 213.4-11.5c23.5-6.3 42-24.2 48.3-47.8 11.4-42.9 11.4-132.3 11.4-132.3s0-89.4-11.4-132.3zm-317.5 213.5V175.2l142.7 81.2-142.7 81.2z",
-    },
-    apple: {
-        viewBox: "0 0 384 512",
-        d: "M318.7 268.7c-.2-36.7 16.4-64.4 50-84.8-18.8-26.9-47.2-41.7-84.7-44.6-35.5-2.8-74.3 20.7-88.5 20.7-15 0-49.4-19.7-76.4-19.7C63.3 141.2 4 184.8 4 273.5q0 39.3 14.4 81.2c12.8 36.7 59 126.7 107.2 125.2 25.2-.6 43-17.9 75.8-17.9 31.8 0 48.3 17.9 76.4 17.9 48.6-.7 90.4-82.5 102.6-119.3-65.2-30.7-61.7-90-61.7-91.9zm-56.6-164.2c27.3-32.4 24.8-61.9 24-72.5-24.1 1.4-52 16.4-67.9 34.9-17.5 19.8-27.8 44.3-25.6 71.9 26.1 2 49.9-11.4 69.5-34.3z"
-    },
-    tidal: {
-        viewBox: "0 0 176 177",
-        d: "M-0.489,59.215L28.787,88.42l29.276-29.205L28.787,30.011L-0.489,59.215z M58.224,59.215L87.5,88.42l29.276-29.205 L87.5,30.011L58.224,59.215z M146.213,30.011l-29.276,29.205l29.276,29.205l29.276-29.205L146.213,30.011z M58.224,117.784 L87.5,146.989l29.276-29.205L87.5,88.58L58.224,117.784z"
-    },
-}
-
 const botonDisponibles = document.querySelector(".botonDisponibles")
 const botonProximos = document.querySelector(".botonProximos")
 const listaDisponibles = document.querySelector('.listaDisponibles')
@@ -185,92 +160,96 @@ function generarListado() {
         listaDisponibles.appendChild(mensajeListaVaciaDisponibles)
     } else {
         if (contadorTipoMediaDisponible('musica').length > 0) {
-            var titleAreaMusica = document.createElement('h2')
-            titleAreaMusica.classList.add('titleAreaMusica')
-            titleAreaMusica.innerHTML = 'Música'
-            listaDeMusicaDisponibles.appendChild(titleAreaMusica)
-            var musicasDisponibles = document.createElement('div')
-            musicasDisponibles.classList.add('musicasDisponibles')
-            listaDeMusicaDisponibles.appendChild(musicasDisponibles)
+            var titleAreaMusica = document.createElement('h2');
+            titleAreaMusica.classList.add('titleAreaMusica');
+            titleAreaMusica.innerHTML = 'Música';
+            listaDeMusicaDisponibles.appendChild(titleAreaMusica);
+
+            var musicasDisponibles = document.createElement('div');
+            musicasDisponibles.classList.add('musicasDisponibles');
+            listaDeMusicaDisponibles.appendChild(musicasDisponibles);
             listaDisponibles.appendChild(listaDeMusicaDisponibles);
 
             contadorTipoMediaDisponible('musica').forEach(media => {
-                var single = document.createElement('div')
-                single.classList.add('single')
+                var single = document.createElement('div');
+                single.classList.add('single');
 
                 const contenido = `
-                                                <img src="${media.coverURL}">
-                                                    <div>
-                                                        <h3>${media.artistas}</h3>
-                                                        <p>${media.nombreCancion}</p>
-                                                    </div>
-                                                    <div class="plataformas">
-                                                        ${media.plataformasMusica.map(plataforma => {
+                    <img src="${media.coverURL}">
+                    <div>
+                        <h3>${media.artistas}</h3>
+                        <p>${media.nombreCancion}</p>
+                    </div>
+                    <div class="plataformas">
+                        ${media.plataformasMusica.map(plataforma => {
                     const iconoPlataforma = ICONOS[plataforma.nombre.toLowerCase()];
                     if (iconoPlataforma) {
                         return `
-                        <a href="${plataforma.url}" target="_blank" rel="noopener noreferrer">
-                            <svg xmlns="${ICONOS.xmlns}" viewBox="${iconoPlataforma.viewBox}">
-                                <path d="${iconoPlataforma.d}"/></path>
-                            </svg>
-                        </a>
-                        `;
+                                    <a href="${plataforma.url}" target="_blank" rel="noopener noreferrer">
+                                        <svg xmlns="${ICONOS.xmlns}" viewBox="${iconoPlataforma.viewBox}">
+                                            <path d="${iconoPlataforma.d}"/>
+                                        </svg>
+                                    </a>
+                                `;
                     } else {
                         console.error(`La plataforma ${plataforma.nombre.toLowerCase()} no está definida en ICONOS.`);
-                        return ''; // Puedes ajustar esto según tus necesidades
+                        return '';
                     }
                 }).join('')}
-                                                    </div>
-                                                    `
-                single.innerHTML = contenido
+                    </div>
+                `;
 
-                musicasDisponibles.appendChild(single)
-            })
+                single.innerHTML = contenido;
+                musicasDisponibles.appendChild(single);
+            });
         }
 
+
         if (contadorTipoMediaDisponible('video').length > 0) {
-            var titleAreaVideo = document.createElement('h2')
-            titleAreaVideo.classList.add('titleAreaVideo')
-            titleAreaVideo.innerHTML = 'Video'
-            listaDeVideoDisponibles.appendChild(titleAreaVideo)
-            var videosDisponibles = document.createElement('div')
-            videosDisponibles.classList.add('videosDisponibles')
-            listaDeVideoDisponibles.appendChild(videosDisponibles)
+            var titleAreaVideo = document.createElement('h2');
+            titleAreaVideo.classList.add('titleAreaVideo');
+            titleAreaVideo.innerHTML = 'Video';
+            listaDeVideoDisponibles.appendChild(titleAreaVideo);
+
+            var videosDisponibles = document.createElement('div');
+            videosDisponibles.classList.add('videosDisponibles');
+            listaDeVideoDisponibles.appendChild(videosDisponibles);
             listaDisponibles.appendChild(listaDeVideoDisponibles);
 
             contadorTipoMediaDisponible('video').forEach(media => {
-                var singleVid = document.createElement('div')
-                singleVid.classList.add('singleVid')
+                var singleVid = document.createElement('div');
+                singleVid.classList.add('singleVid');
 
                 const contenidoVid = `
-                                                    <img src="${media.coverURL}">
-                                                        <div>
-                                                            <h3>${media.artistas}</h3>
-                                                            <p>${media.nombreCancion}</p>
-                                                        </div>
-                                                        <div class="plataformas">
-                                                            ${media.plataformasVideo.map(plataforma => {
+                    <img src="${media.coverURL}">
+                    <div>
+                        <h3>${media.artistas}</h3>
+                        <p>${media.nombreCancion}</p>
+                    </div>
+                    <div class="plataformas">
+                        ${media.plataformasVideo.map(plataforma => {
                     const iconoPlataforma = ICONOS[plataforma.nombre.toLowerCase()];
                     if (iconoPlataforma) {
                         return `
-                        <a href="${plataforma.url}" target="_blank" rel="noopener noreferrer">
-                            <svg xmlns="${ICONOS.xmlns}" viewBox="${iconoPlataforma.viewBox}">
-                                <path d="${iconoPlataforma.d}"/></path>
-                            </svg>
-                        </a>
-                        `;
+                                    <a href="${plataforma.url}" target="_blank" rel="noopener noreferrer">
+                                        <svg xmlns="${ICONOS.xmlns}" viewBox="${iconoPlataforma.viewBox}">
+                                            <path d="${iconoPlataforma.d}"/>
+                                        </svg>
+                                    </a>
+                                `;
                     } else {
                         console.error(`La plataforma ${plataforma.nombre.toLowerCase()} no está definida en ICONOS.`);
-                        return ''; // Puedes ajustar esto según tus necesidades
+                        return '';
                     }
                 }).join('')}
-                                                        </div>
-                                                        `
-                singleVid.innerHTML = contenidoVid
+                    </div>
+                `;
 
-                videosDisponibles.appendChild(singleVid)
-            })
+                singleVid.innerHTML = contenidoVid;
+                videosDisponibles.appendChild(singleVid);
+            });
         }
+
 
         ajusteAUnaSolaAreaDisponible(listaDisponibles)
 
@@ -296,6 +275,7 @@ generarListado();
 
 const botonDropUpTab = document.querySelector('.botonMostrarOcultar');
 const infoTab = document.querySelector('.infoTab');
+const infoTabMerch = document.querySelector('.infoTabMerch');
 
 botonDropUpTab.addEventListener('click', mostrarOcultarInformacion);
 
@@ -305,31 +285,63 @@ function mostrarOcultarInformacion() {
     infoTab.style.display = infoTab.style.display === 'none' ? 'grid' : 'none';
 }
 
+// Solo si los articulos de mercaderia es un numero impar, el ultimo articulo tomara dos columnas en su lugar
+
+function ajustedeColumnas(i) {
+    if (i.childNodes.length % 2 === 1) {
+        i.childNodes[i.childNodes.length - 1].classList.add("ultimoImpar")
+    }
+}
+
 // Carrouseles en Merch
 
 function crearCarrouselesMercaderia() {
-    var infoTabMerch = document.querySelector('.infoTabMerch');
 
     MERCANCIAS.forEach(mercancia => {
         var imagenIndex = 0;
 
-        const contenidoMerch = `
-                                                        <div class="carrousel">
-                                                            <h2>${mercancia.nombreArticulo}</h2>
-                                                            <div class="slides">
-                                                                <img src="${mercancia.imagenesArticulo[imagenIndex]}">
-                                                                    <a class="prevImagen">❮</a>
-                                                                    <a class="nextImagen">❯</a>
-                                                                    <div class="boxDotIndex"></div>
-                                                            </div>
-                                                            <a class="botonComprar" href="${mercancia.enlaceComprar}">
-                                                                Comprar/Buy
-                                                            </a>
-                                                        </div>
-                                                        `;
+        const carrouselDiv = document.createElement("div");
+        carrouselDiv.classList.add("carrousel");
 
-        infoTabMerch.innerHTML += contenidoMerch;
+        const h2Element = document.createElement("h2");
+        h2Element.innerHTML = mercancia.nombreArticulo;
+
+        const slidesDiv = document.createElement("div");
+        slidesDiv.classList.add("slides");
+
+        const imgElement = document.createElement("img");
+        imgElement.src = mercancia.imagenesArticulo[imagenIndex];
+
+        const prevButton = document.createElement("a");
+        prevButton.classList.add("prevImagen");
+        prevButton.textContent = "❮";
+
+        const nextButton = document.createElement("a");
+        nextButton.classList.add("nextImagen");
+        nextButton.textContent = "❯";
+
+        const boxDotIndexDiv = document.createElement("div");
+        boxDotIndexDiv.classList.add("boxDotIndex");
+
+        slidesDiv.appendChild(imgElement);
+        slidesDiv.appendChild(prevButton);
+        slidesDiv.appendChild(nextButton);
+        slidesDiv.appendChild(boxDotIndexDiv);
+
+        const comprarButton = document.createElement("a");
+        comprarButton.classList.add("botonComprar");
+        comprarButton.href = mercancia.enlaceComprar;
+        comprarButton.textContent = "Comprar/Buy";
+
+        carrouselDiv.appendChild(h2Element);
+        carrouselDiv.appendChild(slidesDiv);
+        carrouselDiv.appendChild(comprarButton);
+
+        infoTabMerch.appendChild(carrouselDiv);
     });
+
+    ajustedeColumnas(infoTabMerch)
+
 
     // Obtén todas las instancias de carrousel después de agregarlos al DOM
     var carrousels = document.querySelectorAll('.carrousel');
